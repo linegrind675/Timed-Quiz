2f3cc721203503e3120f737dc477cf5803bba667
Linegrind <linegrind@icloud.com>
2021-02-28 13:56:37 -0700

    timer function

--- assets/script.js
+++ assets/script.js
@@ -113,3 +113,23 @@ var questions = [{
 
 startButton.addEventListener('click', startGame);
 
+function timerStart(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function() {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+        if(seconds === 0) {
+            alert('Game Over')
+        }
+    }, 1000);
+}
+
